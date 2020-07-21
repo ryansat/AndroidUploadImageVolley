d5e77592eaaa34ddfa4b907184e3404a1b98fc4d
Fajar Satria <fajarsat@hotmail.com>
2020-07-22 00:12:52 +0700

    init file

---
+++ app/src/main/java/net/simplifiedlearning/androiduploadimage/EndPoints.java
@@ -0,0 +1,7 @@
+package net.simplifiedlearning.androiduploadimage;
+
+public class EndPoints {
+    private static final String ROOT_URL = "http://10.0.2.2/MyApi/Api.php?apicall=";
+    public static final String UPLOAD_URL = ROOT_URL + "uploadpic";
+    public static final String GET_PICS_URL = ROOT_URL + "getpics";
+}
